c29359aa7e8a1475c25c43ea4f518957585ff95b
NameTakenBonk <83465599+NameTakenBonk@users.noreply.github.com>
2022-07-27 21:22:25 +0100

    Fixed

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ print("You will have to run this every time roblox updates. I might be able to m
 
 def deleteFile(dirSounds):
     if os.path.exists(dirSounds):
-        os.remove(dirRoblox+"\\content\\sounds\\ouch.ogg")
-        print("Removed new Ouch sound")
-    else:
-        print("The file does not exist")
+        if os.path.exists(dirSounds+"\\content\\sounds\\ouch.ogg"):
+            os.remove(dirSounds+"\\content\\sounds\\ouch.ogg")
+            print("Removed new Ouch sound")
+        else:
+            print("The file does not exist")
 
 dirRoblox = input("Dir: \n")
 deleteFile(dirRoblox)
